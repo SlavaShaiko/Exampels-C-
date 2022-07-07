bc3befbb8a812f48e419a75e672544e63b00a210
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-07-07 12:29:08 +0300

    Exampels005-BaseKettlebells

---
+++ Exampels005-BaseKettlebells/Program.cs
@@ -0,0 +1,27 @@
+﻿double a = new Random().Next(1,10);
+Console.Write(a);
+Console.Write('-');
+double b = new Random().Next(1,10);
+Console.Write(b);
+Console.Write('-');
+double c = new Random().Next(1,10);
+Console.Write(c);
+Console.Write('-');
+double d = new Random().Next(1,10);
+Console.Write(d);
+Console.Write('-');
+double e = new Random().Next(1,10);
+Console.Write(e);
+
+double max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("  max =  "); 
+Console.WriteLine(max);
+
+    
