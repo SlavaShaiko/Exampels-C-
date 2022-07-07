3e719099baa2ac79f3c72fbd514aa3afee0f6764
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-07-07 15:54:04 +0300

    Exampels006-While

--- Exampels006-While/Program.cs
+++ Exampels006-While/Program.cs
@@ -2,53 +2,45 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("@");
 
-int xa = 1, ya = 1,
-    xb = 1,  yb = 30,
-    xc = 70, yc = 30,
-    xd = 70, yd = 1;
+int xa = 45, ya = 1,
+    xb = 1, yb = 30,
+    xc = 90, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("A");
+Console.WriteLine(".");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("A");
+Console.WriteLine(".");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("A");
+Console.WriteLine(".");
 
-Console.SetCursorPosition(xd, yd);
-Console.WriteLine("A");
-
-int x = xa, y = xb, z = xd;
+int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 500)
+while(count < 1000)
 {
-    int what = new Random().Next(0, 3); // 0 1 2 3
+    int what = new Random().Next(0, 3); // 0 1 2
     if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
-        z = (z + xa) / 2;
     }
 
     if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
-        z = (z + xb) / 2;
     }
 
     if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
-        z = (z + xc) / 2;
     }
- 
-    
-    Console.SetCursorPosition(x, y, z);
+
+    Console.SetCursorPosition(x, y);
     Console.WriteLine(".");
     count ++;
 }
