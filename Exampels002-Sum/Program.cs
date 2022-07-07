ad1aff9cf06903d5713c16cd6c0a633341657312
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-07-07 11:39:20 +0300

    Exampels002-Sum_edit

--- Exampels002-Sum/Program.cs
+++ Exampels002-Sum/Program.cs
@@ -1,7 +1,7 @@
-﻿int numbersA = 2;
+﻿int numbersA = new Random().Next(1,10);// 1 2 3 4....9
 Console.Write(numbersA);
 Console.Write("+");
-int numbersB = 5;
+int numbersB = new Random().Next(1,10);// 1 2 3 4....9
 Console.Write(numbersB);
 Console.Write("=");
 Console.WriteLine(numbersA + numbersB);
