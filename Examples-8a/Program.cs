d4effb1e3f68374d09d5d4244d7abf28753d4b81
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-10 13:56:44 +0300

    run examples8a

--- Examples-8a/Program.cs
+++ Examples-8a/Program.cs
@@ -1,5 +1,17 @@
 ﻿/*Задача 8. Найти максимальное значение 
-из 8 введённычх чисел(СТИХИЙНО)*/
+из 8 введённычх чисел(С ПОМОЩЬЮ ФУНКЦИИ)*/
+
+//int Max - функция 
+
+/*int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result; 
+}
+*/
+
 
 Console.Clear();
 Console.Write("Введите a1= ");
@@ -21,18 +33,23 @@ int b3 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите c3= ");
 int c3 = Convert.ToInt32(Console.ReadLine());
 
-int max = a1;
 
-if(b1 > max) max = b1;
-if(c1 > max) max = c1;
-
-if(a2 > max) max = a2;
-if(b2 > max) max = b2;
-if(c2 > max) max = c2;
-
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result; 
+}
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine($"максимальлное число из введённых= {max}");
+
 
