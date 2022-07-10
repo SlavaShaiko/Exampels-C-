31227a34fa9acbe3c06aa6b2c0f88fce93e6e370
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-10 18:15:43 +0300

    does not work

---
+++ Examples-ArrayRandom/Program.cs
@@ -0,0 +1,52 @@
+﻿
+/*массив с рандомными значениями*/
+
+Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+    }
+}
+
+void PrintArray(int[] coll) //void(медотд)который не возвращает значение, а return возвращает значение
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // останавливает перебор if
+        }
+    }
+    index++;
+    return position; //(медотд)который возвращает значение
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 2);
+Console.WriteLine(pos);
